790e21da80183d7e11f1578f8810c9e809682db7
Harshitha V <147632098+Harshitha0725@users.noreply.github.com>
2023-11-04 10:14:00 +0530

    Add files via upload

---
+++ Products.js
@@ -0,0 +1,70 @@
+/*============ Build our products data ==============*/
+
+export const productsData = [
+  {
+    id: 1,
+    title: "Om Shirt",
+    price: 350,
+    imageUrl:
+      "./images/1.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Lord Ganesh",
+    price: 400,
+    imageUrl: "./images/2.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Guru Adi-Shankaracharya",
+    price: 400,
+    imageUrl: "./images/3.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Hanuman",
+    price: 395,
+    imageUrl: "./images/hanuman.webp",
+  },
+
+  {
+    id: 5,
+    title: "Lord Rama and Pinaka",
+    price: 350,
+    imageUrl:
+      "./images/pinaka.webp",
+  },
+
+  {
+    id: 6,
+    title: "Lord Radhe Krishna",
+    price: 450,
+    imageUrl: "./images/radhe.webp",
+  },
+
+  {
+    id: 7,
+    title: "Lord Rama",
+    price: 500,
+    imageUrl: "./images/ram.webp",
+  },
+
+  {
+    id: 8,
+    title: "Lord Shiva",
+    price: 500,
+    imageUrl: "./images/shiva.webp",
+  },
+
+  {
+    id: 9,
+    title: "Siya Ram",
+    price: 450,
+    imageUrl: "./images/siya-ram.webp",
+  },
+
+  
+];
